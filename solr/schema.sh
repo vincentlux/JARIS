bcf63c88893f490573dfb778b2da829c0a967678
vincentlunc <xiaopeng@live.unc.edu>
2019-04-07 02:06:28 -0400

    modify field type

--- solr/schema.sh
+++ solr/schema.sh
@@ -1,2 +1,2 @@
-curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"doc_id","type":"text_general","stored":true }}' http://104.248.61.45:8983/solr/jaris/schema
-curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"content","type":"string","stored":true }}' http://104.248.61.45:8983/solr/jaris/schema
+curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"doc_id","type":"string","stored":true }}' http://104.248.61.45:8983/solr/jaris/schema
+curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"content","type":"text_general","stored":true }}' http://104.248.61.45:8983/solr/jaris/schema
