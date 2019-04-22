1442e59ab291294dcd134f27993133dfa7815ec3
Xiaopeng Lu <vincentlu073@gmail.com>
2019-04-22 22:20:19 +0000

    modify field

--- solr/schema.sh
+++ solr/schema.sh
@@ -1,2 +1,2 @@
-curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"doc_id","type":"string","stored":true }}' http://104.248.61.45:8983/solr/jaris-large/schema
-curl -X POST -H 'Content-type:application/xml' --data-binary '{"add-field":{"name":"content","type":"text_general","stored":true }}' http://104.248.61.45:8983/solr/jaris-large/schema
+curl -X POST -H 'Content-type:application/xml' --data-binary '{"replace-field":{"name":"doc_id","type":"string","stored":true }}' http://104.248.61.45:8983/solr/jaris-large/schema
+curl -X POST -H 'Content-type:application/xml' --data-binary '{"replace-field":{"name":"content","type":"text_general","stored":true }}' http://104.248.61.45:8983/solr/jaris-large/schema
